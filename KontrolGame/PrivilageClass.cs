232e1d2c654cbcc7d0625ceb949edccdf82f035f
voda2600 <voda2600@mail.ru>
2021-05-11 20:49:58 +0300

    Ready0.5

---
+++ KontrolGame/PrivilageClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KontrolGame
+{
+    public static class PrivilageClass
+    {
+        public static string GetPrivilage(int kol)
+        {
+            if (kol <= 1) return "Normal";
+            else if (kol <= 6) return "Great";
+            else return "Boss";
+        }
+    }
+}
