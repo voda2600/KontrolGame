9912232cb0126eee4d0f86fbb08aeb8bb0ea4b0c
voda2600 <voda2600@mail.ru>
2021-05-11 20:26:24 +0300

    SmellReady

---
+++ KontrolGame/Order.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace KontrolGame
+{
+    public class Order
+    {
+        public int NumberOfProd { get; }
+        public string  PrivilageOfOrder{get;}
+
+        public Customer _customer;
+        public Order (int kol, Customer customer)
+        {
+            _customer = customer;
+            NumberOfProd = kol;
+            PrivilageOfOrder = Customer.GetPrivilage(_customer.Orders.Sum() + NumberOfProd);
+        }
+
+
+        public int PercentOfSale()
+        {
+           
+            if (PrivilageOfOrder == "Great") return 10;
+            else if (PrivilageOfOrder == "Boss") return 20;
+            else return 0;
+        }
+
+    }
+}
---
+++ KontrolGame/Program.cs
@@ -0,0 +1,12 @@
+﻿    using System;
+
+namespace KontrolGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
